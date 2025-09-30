2c4164400746f161facb3d62aeb39561679cc3b9
luiscarrillo7 <archivo.luis.carrillo@gmail.com>
2025-09-30 09:01:20 -0500

    Configurar adapter vercel

--- src/routes/inventario/+page.svelte
+++ src/routes/inventario/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import IngresoMedicamento from '$components/ingreso-medicamento.svelte';
+    import IngresoMedicamento from '$lib/components/ingreso-medicamento.svelte';
 </script>
 
 <IngresoMedicamento />
