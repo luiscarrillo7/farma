6afc6e60771c6e78a85de6109c490d02096df5ab
luiscarrillo7 <archivo.luis.carrillo@gmail.com>
2025-09-30 10:58:01 -0500

    Configurar adapter vercel

--- src/routes/inventario/+page.svelte
+++ src/routes/inventario/+page.svelte
@@ -1,7 +1,15 @@
 <script>
   import IngresoMedicamento from '$lib/components/ingreso-medicamento.svelte';
   import ListarLotes from '$lib/components/listar-lotes.svelte';
+
+  let refrescar = false;
+
+  function recargarLotes() {
+    refrescar = !refrescar; // cambia estado para forzar actualización en la tabla
+  }
 </script>
 
-<IngresoMedicamento />
-<ListarLotes />
+<div class="space-y-8">
+  <IngresoMedicamento on:loteAgregado={recargarLotes} />
+  <ListarLotes {refrescar} />
+</div>
