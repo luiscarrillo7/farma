b5a4af0930a1b99b4398a45bf2b5d8fcc5bb12fc
luiscarrillo7 <archivo.luis.carrillo@gmail.com>
2025-10-01 18:58:30 -0500

    Configurar adapter vercel

--- src/routes/medicamentos/+page.svelte
+++ src/routes/medicamentos/+page.svelte
@@ -64,7 +64,23 @@
 
 <div class="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
  
-  
+    <header class="bg-blue-800 text-white p-4 shadow-lg flex justify-between items-center">
+    <h1 class="text-2xl font-bold">Inventario</h1>
+
+    <div class="flex gap-4">
+      <button 
+        onclick={irDashboard} 
+        class="bg-green-500 hover:bg-green-600 font-semibold py-2 px-4 rounded-lg">
+        Dashboard
+      </button>
+
+      <button 
+        onclick={logout} 
+        class="bg-red-500 hover:bg-red-600 font-semibold py-2 px-4 rounded-lg">
+        Salir
+      </button>
+    </div>
+  </header>
   <!-- Contenido principal -->
   <main class="max-w-7xl mx-auto p-6 md:p-8 space-y-8">
     {#if session}
