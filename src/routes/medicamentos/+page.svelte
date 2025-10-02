27a68d03e8537bbca6be4feda7ef9ff3a4c78984
luiscarrillo7 <archivo.luis.carrillo@gmail.com>
2025-10-01 19:03:49 -0500

    Configurar adapter vercel

--- src/routes/medicamentos/+page.svelte
+++ src/routes/medicamentos/+page.svelte
@@ -69,9 +69,14 @@
 
     <div class="flex gap-4">
       <button 
-        onclick={irDashboard} 
+        onclick={irInventario} 
         class="bg-green-500 hover:bg-green-600 font-semibold py-2 px-4 rounded-lg">
-        Dashboard
+        Inventario
+      </button>
+      <button 
+        onclick={irMedicamentos} 
+        class="bg-green-500 hover:bg-green-600 font-semibold py-2 px-4 rounded-lg">
+        Medicamentos
       </button>
 
       <button 
